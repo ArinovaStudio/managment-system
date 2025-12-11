e08f08d46edc3fe9f7d1fd571bb0ab176734bf38
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2025-12-11 22:27:39 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,26 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      crypto: false,
+      stream: false,
+      util: false,
+      buffer: false,
+      encoding: false,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
